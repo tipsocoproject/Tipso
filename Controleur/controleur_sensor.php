ee4bd860864f39a0e0fbe8d56fb445cfd36bc835
doooud <philippick.a@gmail.com>
2017-01-22 16:37:24 +0100

    Finalisation du back sur Sensor

---
+++ Controleur/controleur_sensor.php
@@ -0,0 +1,56 @@
+<?php 
+
+if(isset($_SESSION['id']))
+{
+    $idc = $_SESSION['id'];
+
+    if(isset($_POST['confirm-button']))
+    {   
+        $counter = count($_POST['serialnumber']);
+
+        for ($i = 0; $i <= $counter-1; $i++) {
+
+            $serialnumber = uninjection_sql(htmlspecialchars($_POST['serialnumber'][$i]));
+            $selectroom = uninjection_sql(htmlspecialchars($_POST['selectroom'][$i]));
+            $name = uninjection_sql(htmlspecialchars($_POST['room'][$i]));
+
+
+
+            $reqserial = $bdd->prepare("SELECT * FROM sensors WHERE sensorserial = ?");
+            $reqserial->execute(array($serialnumber));
+            $serialidc = $reqserial->fetch();
+            $serialexist = $reqserial->rowCount();
+
+            $idsens = $serialidc['idsens'];
+
+
+
+            if($serialidc['idc'] == 0)
+            {
+                if($serialexist == 1)
+                {  
+                    $updatesens = $bdd->prepare("UPDATE sensors SET idc = ?, mail=? WHERE  sensorserial = ?");
+                    $updatesens->execute(array($idc, $_SESSION['mail'], $serialnumber));
+
+                    $inserroom = $bdd->prepare("INSERT INTO rooms(idc, roomname, type, idsens) VALUES(?,?,?,?)");
+                    $inserroom->execute(array($idc, $name, $selectroom, $idsens));
+                }
+                else
+                {
+                    $error= "Ce numéro de série n'existe pas !";
+                }
+            }
+            else
+            {
+                $error = "Objet déjà enregistré !";
+
+            }
+        }
+
+    }
+}
+else
+{
+    header('Location: login.php');
+}
+?>
--- Vue/sensor.php
+++ Vue/sensor.php
@@ -1,63 +1,29 @@
 <?php
+session_start();
+require("../Modele/connexion.php");
 include("../fonction/function_uninjection_sql.php");
+include("../Controleur/controleur_sensor.php");
 
 
-if(isset($_POST['confirm-button']))
-{
-    for ($i = 0; $i <= 10; $i++) {
-        
-            $serialnumber = uninjection_sql(htmlspecialchars($_POST['serialnumber'][$i]));
-            echo $serialnumber;
-        
-  
-    /*
 
-    if(!empty($_POST['email']) AND !empty($_POST['password']))
-    {
-        $requser = $bdd->prepare("SELECT * FROM client WHERE mail = ? AND password = ?");
-        $requser->execute(array($mail, $password));
-        $userexist = $requser->rowCount();
-    */
-    }
-
-}
 ?>
 
 <html>
 <head>
     <meta charset="utf-8" />
-    <title>Rooms</title>
-
-    <style>
-
-    img {
-        cursor: pointer;
-        border-width: 1px;
-        height: 50px;
-        width: 50px;
-        margin-left: 30px;
-        margin-right: 30px; 
-    }
-
-    input {
-    	margin-left: 20px;
-    	margin-right: 20px;
-        margin-bottom: 20px;
-    	width: 300px;
-    }
-
-    </style>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../Styles/sensor.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js">
-    </script>
+    </script>   
+    <title>Ajout produit</title>
 </head>
+
 <body>
     <div class="sensors-select" align="center">
         <form method="POST" action="">
             <img src="http://www.icone-png.com/png/30/29916.png" id="plus">
             <script>
 
-            var total_room = ["Salle à manger","Salon","Entrée","Salle de bain","Cuisine","Terrasse","Bureau","Buanderie","Autres"];
             var counter = 0;
 
             /* When the document is charged the function begins.*/
@@ -66,19 +32,21 @@ if(isset($_POST['confirm-button']))
 
                 $('#plus').click(function() {
                     counter++;
-                    $('#plus').before('<div class="image"><input type="text" name="serialnumber[]" required/><select name="selectroom[]" required/></select><input type="text" name="room[]" required/></div>');
-                    for (i = 0; i < total_room.length; i++) { 
-                        $('select[name$="selectroom[]"]').append($('<option>', {
-                            value: i,
-                            text: total_room[i]
-                        }));
-                    }
+                    $('#plus').before('<div class="image"><input type="text" name="serialnumber[]" required/><select name="selectroom[]" required><option>Entrée</option><option>Salon</option><option>Chambre</option><option>Cuisine</option><option>Buanderie</option><option>Terrasse</option><option>Salle à manger</option><option>Autres</option></select><input type="text" name="room[]" required/></div>');
                 });
-            });
+            })
+
+
 
         </script>
         <input class="button" type="submit" name="confirm-button">
         </form>
+        <span class="error">
+            <?php
+                require("../Modele/error.php");
+            ?>
+        </span> 
     </div>
 </body>
 </html>
+
