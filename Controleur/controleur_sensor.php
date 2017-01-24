f624ede5741a5d58e7a1a971424130121b1b7fb6
doooud <philippick.a@gmail.com>
2017-01-24 17:16:11 +0100

    Ajout cas de figure nul controleur_sensor

--- Controleur/controleur_sensor.php
+++ Controleur/controleur_sensor.php
@@ -6,47 +6,62 @@ if(isset($_SESSION['id']))
 
     if(isset($_POST['confirm-button']))
     {   
-        $counter = count($_POST['serialnumber']);
+        if(isset($counter))
+        {              
+            $counter = count($_POST['serialnumber']);
+        }
+        else
+        {
+            $counter = 0;
+        }
 
-        for ($i = 0; $i <= $counter-1; $i++) {
+        if($counter!==0)
+        {
 
-            $serialnumber = uninjection_sql(htmlspecialchars($_POST['serialnumber'][$i]));
-            $selectroom = uninjection_sql(htmlspecialchars($_POST['selectroom'][$i]));
-            $name = uninjection_sql(htmlspecialchars($_POST['room'][$i]));
+            for ($i = 0; $i <= $counter-1; $i++) {
 
-
-
-            $reqserial = $bdd->prepare("SELECT * FROM sensors WHERE sensorserial = ?");
-            $reqserial->execute(array($serialnumber));
-            $serialidc = $reqserial->fetch();
-            $serialexist = $reqserial->rowCount();
-
-            $idsens = $serialidc['idsens'];
+                $serialnumber = uninjection_sql(htmlspecialchars($_POST['serialnumber'][$i]));
+                $selectroom = uninjection_sql(htmlspecialchars($_POST['selectroom'][$i]));
+                $name = uninjection_sql(htmlspecialchars($_POST['room'][$i]));
 
 
-            if($serialidc['idc'] == 0)
-            {
-                if($serialexist == 1)
-                {  
-                    $updatesens = $bdd->prepare("UPDATE sensors SET idc = ?, mail=? WHERE  sensorserial = ?");
-                    $updatesens->execute(array($idc, $_SESSION['mail'], $serialnumber));
 
-                    $inserroom = $bdd->prepare("INSERT INTO rooms(idc, roomname, type, idsens) VALUES(?,?,?,?)");
-                    $inserroom->execute(array($idc, $name, $selectroom, $idsens));
+                $reqserial = $bdd->prepare("SELECT * FROM sensors WHERE sensorserial = ?");
+                $reqserial->execute(array($serialnumber));
+                $serialidc = $reqserial->fetch();
+                $serialexist = $reqserial->rowCount();
+
+                $idsens = $serialidc['idsens'];
+
+
+                if($serialidc['idc'] == 0)
+                {
+                    if($serialexist == 1)
+                    {  
+                        $updatesens = $bdd->prepare("UPDATE sensors SET idc = ?, mail=? WHERE  sensorserial = ?");
+                        $updatesens->execute(array($idc, $_SESSION['mail'], $serialnumber));
+
+                        $inserroom = $bdd->prepare("INSERT INTO rooms(idc, roomname, type, idsens) VALUES(?,?,?,?)");
+                        $inserroom->execute(array($idc, $name, $selectroom, $idsens));
+                    }
+                    else
+                    {
+                        $error= "Ce numéro de série n'existe pas !";
+                    }
                 }
                 else
                 {
-                    $error= "Ce numéro de série n'existe pas !";
+                    $error = "Objet déjà enregistré !";
+
                 }
             }
-            else
-            {
-                $error = "Objet déjà enregistré !";
-
-            }
         }
-
+        else
+        {
+            $error = "Rien n'a été ajouté";
+        }
     }
+
 }
 else
 {
