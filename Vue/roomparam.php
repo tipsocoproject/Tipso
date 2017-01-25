f89dc629b760d8021f7fd610c2f8c715a0808b64
doooud <philippick.a@gmail.com>
2017-01-25 12:17:09 +0100

    roomparam commit

--- Vue/roomparam.php
+++ Vue/roomparam.php
@@ -13,6 +13,7 @@ include("../Controleur/controleur_roomparam.php");
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="../Styles/temperature.css">
 	<link rel="stylesheet" type="text/css" href="../Styles/header_footer_for_connected_style.css">
+	<link rel="stylesheet" type="text/css" href="../Styles/button.css">
 	<link rel="stylesheet" type="text/css" href="../Styles/roomparam.style.css">
 
 
@@ -53,14 +54,54 @@ include("../Controleur/controleur_roomparam.php");
 						$reqsens->execute(array($array_roomidsens[$i]));
 
 						$idname = $reqsens->fetch();
-
+						$onval = 0;
+						$movval = 0;
+						$tempval = 25;
 
 
 						echo '<tr>';
 							echo '<td><br><input class="input-box" type="text" name="state" value="" disabled placeholder="  '.$title.'"></td>';
-							echo '<td><br><input class="input-box" type="text" name="modification" value="" disabled placeholder="  '.$array_roomname[$i].'"></td>';
+							echo '<td><br><input class="input-box" type="text" name="modification" value="" disabled placeholder="  '.$array_roomname[$i].'"></td>';				
 							echo '<td><br><input class="input-box" type="text" name="state" value="" disabled placeholder=  '.$idname['sensortype'].'></td>';
-							echo '<td><br><input class="input-box" type="text" name="modification" value=""></td>';
+							if($idname['sensortype']=="Température")
+							{
+								if($tempval != 0)
+								{
+									echo '<td><br><select class="input-box" name="modification"><option selected="selected">'.$tempval."°C".'</option><option>15°C</option><option>16°C</option><option>17°C</option><option>18°C</option><option>19°C</option><option>20°C</option><option>21°C</option><option>22°C</option><option>23°C</option><option>24°C</option><option>25°C</option><option>26°C</option><option>27°C</option><option>28°C</option><option>29°C</option><option>30°C</option></select></td>';
+								}
+								else
+								{
+									echo '<td><br><select class="input-box" name="modification" disabled><option selected="selected">&nbsp</option><option>15</option><option>16</option><option>17</option><option>18</option><option>19</option><option>20</option><option>21</option><option>22</option><option>23</option><option>24</option><option>25</option><option>26</option><option>27</option><option>28</option><option>29</option><option>30</option></select></td>';
+								}
+							}
+							elseif($idname['sensortype']=="Mouvement")
+							{
+								if($movval == 0)
+								{
+									echo '<td><br><input class="input-box" type="text" name="state" value="" placeholder="  Vide"></td>';
+								}
+								else
+								{
+									echo '<td><br><input class="input-box" type="text" name="state" value="" placeholder=" Occupé"></td>';
+
+								}
+							}
+							elseif($idname['sensortype']=="Lumière")
+							{
+								if($onval == 0)
+								{
+									echo '<td><label style="position:relative; left: 3.5vh;" class="switch"><input name="switch[]" type="checkbox" class="checkbox"/><div class=""></div></label></td>';
+								}
+								else
+								{
+									echo '<td><label class="switch"><input name="switch[]" type="checkbox" class="checkbox" checked="checked"/><div class=""></div></label></td>';
+
+								}
+							}
+							else
+							{
+								echo '<td><br><input class="input-box" type="text" name="modification" value=""></td>';
+							}
 							echo '<td><br><input class="input-box" type="text" name="modification" value=""></td>';
 						echo '</tr>';
 
