fb65dadd137dd820279cf4641118fceb6da3e3f8
FengDavid96 <fyhdavid@gmail.com>
2017-01-26 12:04:25 +0100

    graphbis

--- Vue/graph.php
+++ Vue/graph.php
@@ -1,11 +1,13 @@
 <?php
 require('../Modele/connexion.php');
+
 //query to get data from the table
 if (isset($_POST['confirm-button'])){
 	$idsens = intval($_POST['capteur']);
 	$date = htmlspecialchars($_POST['date']);
+	$capteur = htmlspecialchars($_POST['capteur']);
 
-	$querydatasent = $bdd->prepare(sprintf("SELECT  datasent FROM data WHERE idsens=? AND date=? ORDER BY date,time ASC"));
+	$querydatasent = $bdd->prepare(sprintf("SELECT datasent FROM data WHERE idsens=? AND date=? ORDER BY date,time ASC"));
 	$querytime = $bdd->prepare(sprintf("SELECT time FROM data WHERE idsens=? AND date=? ORDER BY date,time ASC"));
 
 
@@ -34,6 +36,37 @@ if (isset($_POST['confirm-button'])){
 	$querytime->closeCursor();
 	$querydatasent->closeCursor();
 }
+
+	//print list date
+	$reqdate = $bdd->prepare(sprintf("SELECT DISTINCT date FROM data ORDER BY date"));
+	$reqdate->execute(array());
+
+	$resultdate = $reqdate->fetchAll(PDO::FETCH_COLUMN);
+
+	$listedate = array();
+
+	for ($i=0; $i < count($resultdate); $i++)
+	{ 
+		$listedate[$i] = $resultdate[$i];
+	}
+
+	$reqdate->closeCursor();
+
+
+	//print list capteur
+	$reqcapteur = $bdd->prepare(sprintf("SELECT DISTINCT idsens FROM data ORDER BY idsens ASC"));
+	$reqcapteur->execute(array());
+
+	$resultcapteur = $reqcapteur->fetchAll(PDO::FETCH_COLUMN);
+
+	$listecapteur = array();
+
+	for ($i=0; $i < count($resultcapteur); $i++)
+	{ 
+		$listecapteur[$i] = $resultcapteur[$i];
+	}
+
+	$reqcapteur->closeCursor();
 ?>
 
 <html>
@@ -48,104 +81,221 @@ if (isset($_POST['confirm-button'])){
 		<script src="../Javascript/jquery.js"></script>
 		<script src="../Javascript/Chart.js"></script>
 
+		<!-- TEMPERATURE -->
 		<script>
-		'use strict'
+			'use strict'
 
-$(function () {
+			$(function () {
 
-	// Step 3. Create a data object
-	var data = {
-	    labels: <?php print json_encode($datatime);?>,
-	    datasets: [
-	        /*{
-	            label: "The First Dataset",
-	            fillColor: "rgba(153,0,76,0.2)", // magenta
-	            strokeColor: "rgba(153,0,76,1)", // magenta
-	            pointColor: "rgba(153,0,76,1)", // magenta
-	            pointStrokeColor: "#fff", // white
-	            pointHighlightFill: "#fff", // white
-	            pointHighlightStroke: "rgba(153,0,76,1)", // magenta
-	            data: [100, 34, 21, 56, 23, 90, 40]
-	        },*/
-	        {
-	            label: "The Second dataset",
-	            fillColor: "rgba(76,0,153,0.2)",
-	            strokeColor: "rgba(76,0,153,1)",
-	            pointColor: "rgba(76,0,153,1)",
-	            pointStrokeColor: "#fff", // white
-	            pointHighlightFill: "#fff", // white
-	            pointHighlightStroke: "rgba(76,0,153,1)",
-	            data: <?php print json_encode($data);?>
-	        }
-	    ]
-	};
+				// Step 3. Create a data object
+				var data = {
+				    labels: <?php print json_encode($datatime);?>,
+				    datasets: [
+				        /*{
+				            label: "The First Dataset",
+				            fillColor: "rgba(153,0,76,0.2)", // magenta
+				            strokeColor: "rgba(153,0,76,1)", // magenta
+				            pointColor: "rgba(153,0,76,1)", // magenta
+				            pointStrokeColor: "#fff", // white
+				            pointHighlightFill: "#fff", // white
+				            pointHighlightStroke: "rgba(153,0,76,1)", // magenta
+				            data: [100, 34, 21, 56, 23, 90, 40]
+				        },*/
+				        {
+				            label: "The Second dataset",
+				            fillColor: "rgba(76,0,153,0.2)",
+				            strokeColor: "rgba(76,0,153,1)",
+				            pointColor: "rgba(76,0,153,1)",
+				            pointStrokeColor: "#fff", // white
+				            pointHighlightFill: "#fff", // white
+				            pointHighlightStroke: "rgba(76,0,153,1)",
+				            data: <?php print json_encode($data);?>
+				        }
+				    ]
+				};
 
-	// Step 2. Get the context of the canvas element we want to select
-	var ctx = document.getElementById("myChart").getContext("2d");
-	var myNewChart = new Chart(ctx).Line(data, {
-	    ///Boolean - Whether grid lines are shown across the chart
-	    scaleShowGridLines : true,
+				// Step 2. Get the context of the canvas element we want to select
+				var ctx = document.getElementById("myCharttemp").getContext("2d");
+				var myNewChart = new Chart(ctx).Line(data, {
+				    ///Boolean - Whether grid lines are shown across the chart
+				    scaleShowGridLines : true,
 
-	    //String - Colour of the grid lines
-	    scaleGridLineColor : "rgba(0,0,0,.05)",
+				    //String - Colour of the grid lines
+				    scaleGridLineColor : "rgba(0,0,0,.1)",
 
-	    //Number - Width of the grid lines
-	    scaleGridLineWidth : 1,
+				    //Number - Width of the grid lines
+				    scaleGridLineWidth : 1,
 
-	    //Boolean - Whether to show horizontal lines (except X axis)
-	    scaleShowHorizontalLines: true,
+				    //Boolean - Whether to show horizontal lines (except X axis)
+				    scaleShowHorizontalLines: true,
 
-	    //Boolean - Whether to show vertical lines (except Y axis)
-	    scaleShowVerticalLines: true,
+				    //Boolean - Whether to show vertical lines (except Y axis)
+				    scaleShowVerticalLines: true,
 
-	    //Boolean - Whether the line is curved between points
-	    bezierCurve : true,
+				    //Boolean - Whether the line is curved between points
+				    bezierCurve : true,
 
-	    //Number - Tension of the bezier curve between points
-	    bezierCurveTension : 0.4,
+				    //Number - Tension of the bezier curve between points
+				    bezierCurveTension : 0.4,
 
-	    //Boolean - Whether to show a dot for each point
-	    pointDot : true,
+				    //Boolean - Whether to show a dot for each point
+				    pointDot : true,
 
-	    //Number - Radius of each point dot in pixels
-	    pointDotRadius : 4,
+				    //Number - Radius of each point dot in pixels
+				    pointDotRadius : 4,
 
-	    //Number - Pixel width of point dot stroke
-	    pointDotStrokeWidth : 1,
+				    //Number - Pixel width of point dot stroke
+				    pointDotStrokeWidth : 1,
 
-	    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-	    pointHitDetectionRadius : 20,
+				    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+				    pointHitDetectionRadius : 20,
 
-	    //Boolean - Whether to show a stroke for datasets
-	    datasetStroke : true,
+				    //Boolean - Whether to show a stroke for datasets
+				    datasetStroke : true,
 
-	    //Number - Pixel width of dataset stroke
-	    datasetStrokeWidth : 2,
+				    //Number - Pixel width of dataset stroke
+				    datasetStrokeWidth : 2,
 
-	    //Boolean - Whether to fill the dataset with a colour
-	    datasetFill : true,
-	});
+				    //Boolean - Whether to fill the dataset with a colour
+				    datasetFill : true,
+				});
 
-});
+			});
 		</script>
-		
+
+		<!-- LUMIERE -->
+		<script>
+			'use strict'
+
+			$(function () {
+
+				// Step 3. Create a data object
+				var data = {
+				    labels: <?php print json_encode($datatime);?>,
+				    datasets: [
+				        /*{
+				            label: "The First Dataset",
+				            fillColor: "rgba(153,0,76,0.2)", // magenta
+				            strokeColor: "rgba(153,0,76,1)", // magenta
+				            pointColor: "rgba(153,0,76,1)", // magenta
+				            pointStrokeColor: "#fff", // white
+				            pointHighlightFill: "#fff", // white
+				            pointHighlightStroke: "rgba(153,0,76,1)", // magenta
+				            data: [100, 34, 21, 56, 23, 90, 40]
+				        },*/
+				        {
+				            label: "The Second dataset",
+				            fillColor: "rgba(76,0,153,0.2)",
+				            strokeColor: "rgba(76,0,153,1)",
+				            pointColor: "rgba(76,0,153,1)",
+				            pointStrokeColor: "#fff", // white
+				            pointHighlightFill: "#fff", // white
+				            pointHighlightStroke: "rgba(76,0,153,1)",
+				            data: <?php print json_encode($data);?>
+				        }
+				    ]
+				};
+
+				// Step 2. Get the context of the canvas element we want to select
+				var ctx = document.getElementById("myChartlum").getContext("2d");
+				var myNewChart = new Chart(ctx).Bar(data, {
+				    ///Boolean - Whether grid lines are shown across the chart
+				    scaleShowGridLines : true,
+
+				    //String - Colour of the grid lines
+				    scaleGridLineColor : "rgba(0,0,0,.1)",
+
+				    //Number - Width of the grid lines
+				    scaleGridLineWidth : 1,
+
+				    //Boolean - Whether to show horizontal lines (except X axis)
+				    scaleShowHorizontalLines: true,
+
+				    //Boolean - Whether to show vertical lines (except Y axis)
+				    scaleShowVerticalLines: true,
+
+				    //Boolean - Whether the line is curved between points
+				    bezierCurve : true,
+
+				    //Number - Tension of the bezier curve between points
+				    bezierCurveTension : 0.4,
+
+				    //Boolean - Whether to show a dot for each point
+				    pointDot : true,
+
+				    //Number - Radius of each point dot in pixels
+				    pointDotRadius : 4,
+
+				    //Number - Pixel width of point dot stroke
+				    pointDotStrokeWidth : 1,
+
+				    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+				    pointHitDetectionRadius : 20,
+
+				    //Boolean - Whether to show a stroke for datasets
+				    datasetStroke : true,
+
+				    //Number - Pixel width of dataset stroke
+				    datasetStrokeWidth : 2,
+
+				    //Boolean - Whether to fill the dataset with a colour
+				    datasetFill : true,
+				});
+
+			});
+		</script>
+			
 	</head>
 
 	<body>
 
 		<form method="POST" action="">
-			<canvas id="myChart" height="450" width="600"></canvas>
+			<canvas id="myCharttemp" height="450" width="600"></canvas>
 
 			<select id="date" name="date">
 				<option value="0">-</option>
-				<option value="2017-01-21">2017-01-21</option>
-				<option value="2017-01-22">2017-01-22</option>
+				<?php 
+					for ($i=0; $i<count($listedate); $i++)
+					{
+						echo "<option value=".$listedate[$i]." select='selected'>".$listedate[$i]."</option>";
+					}
+				?>
 			</select>
 
 			<select id="capteur" name="capteur">
 				<option value="0">-</option>
-				<option value="17">17</option>
-				<option value="18">18</option>
+				<?php 
+					for ($i=0; $i<count($listecapteur); $i++)
+					{ 
+						echo "<option value=".$listecapteur[$i]." select='selected'>".$listecapteur[$i]."</option>";
+					}
+				?>
+			</select>
+			
+			<input type="submit" value="Valider" name="confirm-button" id="confirm-button">
+		</form>
+
+		<form method="POST" action="">
+			<canvas id="myChartlum" height="450" width="600"></canvas>
+
+			<select id="date" name="date">
+				<option value="0">-</option>
+				<?php 
+					for ($i=0; $i<count($listedate); $i++)
+					{
+						echo "<option value=".$listedate[$i]." select='selected'>".$listedate[$i]."</option>";
+					}
+				?>
+			</select>
+
+			<select id="capteur" name="capteur">
+				<option value="0">-</option>
+				<?php 
+					for ($i=0; $i<count($listecapteur); $i++)
+					{ 
+						echo "<option value=".$listecapteur[$i]." select='selected'>".$listecapteur[$i]."</option>";
+					}
+				?>
 			</select>
 			
 			<input type="submit" value="Valider" name="confirm-button" id="confirm-button">
---
+++ Vue/randomvaleur.php
@@ -0,0 +1,248 @@
+<?php
+require('../Modele/connexion.php');
+include('../fonction/function_uninjection_sql.php');
+
+function randomTime($start_date, $end_date)
+{
+    // Convert to timetamps
+    $min = strtotime($start_date);
+    $max = strtotime($end_date);
+
+    // Generate random number using above bounds
+    $val = rand($min, $max);
+
+    // Convert back to desired date format
+    return date('H:i:s', $val);
+}
+
+function dateDiff($datefin, $datedebut)
+{
+	$date1 = strtotime($datefin);
+	$date2 = strtotime($datedebut);
+
+    $diff = abs($date1 - $date2); // abs pour avoir la valeur absolute, ainsi éviter d'avoir une différence négative
+    $retour = array();
+ 
+    $tmp = $diff;
+    $retour['second'] = $tmp % 60;
+ 
+    $tmp = floor( ($tmp - $retour['second']) /60 );
+    $retour['minute'] = $tmp % 60;
+ 
+    $tmp = floor( ($tmp - $retour['minute'])/60 );
+    $retour['hour'] = $tmp % 24;
+ 
+    $tmp = floor( ($tmp - $retour['hour'])  /24 );
+    $retour['day'] = $tmp + 2;
+ 
+    return $retour;
+}
+
+function getDatesBetween($start, $end)
+{
+	if($start > $end)
+	{
+		return false;
+	} 
+
+	$sdate = strtotime("$start");
+	$edate = strtotime($end);
+
+	$dates = array();
+
+	for($i = $sdate; $i <= $edate; $i += strtotime('+1 day', 0))
+	{
+		$dates[] = date('Y-m-d', $i);
+	}
+
+	return $dates;
+}
+
+if (isset($_POST['confirm-button']))
+{
+	$idcapteurtemp = uninjection_sql(intval(htmlspecialchars($_POST['idcapteurtemp'])));
+	$idcapteurlum = uninjection_sql(intval(htmlspecialchars($_POST['idcapteurlum'])));
+	$idcapteurhum = uninjection_sql(intval(htmlspecialchars($_POST['idcapteurhum'])));
+	$idcapteurmov = uninjection_sql(intval(htmlspecialchars($_POST['idcapteurmov'])));
+
+	$typetramtemp = uninjection_sql(intval(htmlspecialchars($_POST['typetramtemp'])));
+	$typetramlum = uninjection_sql(intval(htmlspecialchars($_POST['typetramlum'])));
+	$typetramhum = uninjection_sql(intval(htmlspecialchars($_POST['typetramhum'])));
+	$typetrammov = uninjection_sql(intval(htmlspecialchars($_POST['typetrammov'])));
+
+	$datedebuttemp = uninjection_sql($_POST['date-debut-temp']);
+	$datedebutlum = uninjection_sql($_POST['date-debut-lum']);
+	$datedebuthum = uninjection_sql($_POST['date-debut-hum']);
+	$datedebutmov = uninjection_sql($_POST['date-debut-mov']);
+
+	$datefintemp = uninjection_sql($_POST['date-fin-temp']);
+	$datefinlum = uninjection_sql($_POST['date-fin-lum']);
+	$datefinhum = uninjection_sql($_POST['date-fin-hum']);
+	$datefinmov = uninjection_sql($_POST['date-fin-mov']);
+
+	$valeurmintemp = uninjection_sql(intval(htmlspecialchars($_POST['valeur-min-temp'])));
+	$valeurminlum = uninjection_sql(intval(htmlspecialchars($_POST['valeur-min-lum'])));
+	$valeurminhum = uninjection_sql(intval(htmlspecialchars($_POST['valeur-min-hum'])));
+	$valeurminmov = uninjection_sql(intval(htmlspecialchars($_POST['valeur-min-mov'])));
+
+	$valeurmaxtemp = uninjection_sql(intval(htmlspecialchars($_POST['valeur-max-temp'])));
+	$valeurmaxlum = uninjection_sql(intval(htmlspecialchars($_POST['valeur-max-lum'])));
+	$valeurmaxhum = uninjection_sql(intval(htmlspecialchars($_POST['valeur-max-hum'])));
+	$valeurmaxmov = uninjection_sql(intval(htmlspecialchars($_POST['valeur-max-mov'])));
+
+	if (!empty($_POST['idcapteurtemp']) AND !empty($_POST['typetramtemp']) AND !empty($_POST['date-debut-temp']) AND !empty($_POST['date-fin-temp']) AND !empty($_POST['valeur-min-temp']) AND !empty($_POST['valeur-max-temp']))
+	{	
+		$datediff = dateDiff($datefintemp,$datedebuttemp);
+
+		$datebetween = getDatesBetween($datedebuttemp,$datefintemp);
+
+		for ($i=0; $i < $datediff['day']; $i++)
+		{
+			for ($j=0; $j < 24; $j++)
+			{
+				$rantime = randomTime('2017-01-01','2017-01-02');
+				$rantemp = rand($valeurmintemp,$valeurmaxtemp);
+				$req = $bdd->prepare("INSERT INTO data(idsens, typetram, datasent, time, date) VALUES (?,?,?,?,?)");
+				$req->execute(array($idcapteurtemp,$typetramtemp,$rantemp,$rantime,$datebetween[$i]));
+			}
+		}
+		
+	}
+
+	else if (!empty($_POST['idcapteurlum']) AND !empty($_POST['typetramlum']) AND !empty($_POST['date-debut-lum']) AND !empty($_POST['date-fin-lum']))
+	{
+		$datediff = dateDiff($datefinlum,$datedebutlum);
+
+		$datebetween = getDatesBetween($datedebutlum,$datefinlum);
+
+		for ($i=0; $i < $datediff['day']; $i++)
+		{
+			for ($j=0; $j < 24; $j++)
+			{
+				$rantime = randomTime('2017-01-01','2017-01-02');
+				$ranlum = rand(0,1);
+				$req = $bdd->prepare("INSERT INTO data(idsens, typetram, datasent, time, date) VALUES (?,?,?,?,?)");
+				$req->execute(array($idcapteurlum,$typetramlum,$ranlum,$rantime,$datebetween[$i]));
+			}
+		}
+	}
+
+	else if (!empty($_POST['idcapteurhum']) AND !empty($_POST['typetramhum']) AND !empty($_POST['date-debut-hum']) AND !empty($_POST['date-fin-hum']) AND !empty($_POST['valeur-min-hum']) AND !empty($_POST['valeur-max-hum']))
+	{
+		$datediff = dateDiff($datefinhum,$datedebuthum);
+
+		$datebetween = getDatesBetween($datedebuthum,$datefinhum);
+
+		for ($i=0; $i < $datediff['day']; $i++)
+		{
+			for ($j=0; $j < 24; $j++)
+			{
+				$rantime = randomTime('2017-01-01','2017-01-02');
+				$ranhum = rand($valeurminhum,$valeurmaxhum);
+				$req = $bdd->prepare("INSERT INTO data(idsens, typetram, datasent, time, date) VALUES (?,?,?,?,?)");
+				$req->execute(array($idcapteurhum,$typetramhum,$ranhum,$ranhum,$datebetween[$i]));
+			}
+		}
+	}
+
+	else if (!empty($_POST['idcapteurmov']) AND !empty($_POST['typetrammov']) AND !empty($_POST['date-debut-mov']) AND !empty($_POST['date-fin-mov']))
+	{
+		$datediff = dateDiff($datefintemp,$datedebuttemp);
+
+		$datebetween = getDatesBetween($datedebuttemp,$datefintemp);
+
+		for ($i=0; $i < $datediff['day']; $i++)
+		{
+			for ($j=0; $j < 24; $j++)
+			{
+				$rantime = randomTime('2017-01-01','2017-01-02');
+				$ranmov = rand(0,1);
+				$req = $bdd->prepare("INSERT INTO data(idsens, typetram, datasent, time, date) VALUES (?,?,?,?,?)");
+				$req->execute(array($idcapteurmov,$typetrammov,$ranmov,$rantime,$datebetween[$i]));
+			}
+		}
+	}
+}
+
+
+?>
+
+<html>
+
+<head>
+	<meta charset='utf-8'>
+	<title>random valeur</title>
+</head>
+
+<body>
+
+	<form method="POST" action="">
+
+		<div id="temperature" name="temperature">
+			<!-- TEMPERATURE -->
+			<h2>TEMPERATURE</h2>
+			<input type="number" name="idcapteurtemp" id="idcapteurtemp" placeholder="idcapteur">
+
+			<input type="number" name="typetramtemp" id="typetramtemp" placeholder="typetram">
+
+			<input type="date" name="date-debut-temp" id="date-debut-temp" placeholder="date de debut">
+			<input type="date" name="date-fin-temp" id="date-fin-temp" placeholder="date de fin">
+
+			<input type="number" name="valeur-min-temp" id="valeur-min-temp" placeholder="valeur-min">
+			<input type="number" name="valeur-max-temp" id="valeur-max-temp" placeholder="valeur-max">
+
+		</div>
+
+
+		<div id="lumiere" name="lumiere">
+			<!-- LUMIERE -->
+			<h2>LUMIERE</h2>
+			<input type="number" name="idcapteurlum" id="idcapteurlum" placeholder="idcapteur">
+
+			<input type="number" name="typetramlum" id="typetramlum" placeholder="typetram">
+
+			<input type="date" name="date-debut-lum" id="date-debut-lum" placeholder="date de debut">
+			<input type="date" name="date-fin-lum" id="date-fin-lum" placeholder="date de fin">
+
+		</div>
+
+
+		<div id="humidite" name="humidite">
+			<!-- HUMIDITE -->
+			<h2>HUMIDITE</h2>
+			<input type="number" name="idcapteurhum" id="idcapteurhum" placeholder="idcapteur">
+
+			<input type="number" name="typetramhum" id="typetramhum" placeholder="typetram">
+
+			<input type="date" name="date-debut-hum" id="date-debut-hum" placeholder="date de debut">
+			<input type="date" name="date-fin-hum" id="date-fin-hum" placeholder="date de fin">
+
+			<input type="number" name="valeur-min-hum" id="valeur-min-hum" placeholder="valeur-min">
+			<input type="number" name="valeur-max-hum" id="valeur-max-hum" placeholder="valeur-max">
+
+		</div>
+
+
+		<div id="mouvement" name="mouvement">
+			<!-- MOUVEMENT -->
+			<h2>MOUVEMENT</h2>
+			<input type="number" name="idcapteurmov" id="idcapteurmov" placeholder="idcapteur">
+
+			<input type="number" name="typetrammov" id="typetrammov" placeholder="typetram">
+
+			<input type="date" name="date-debut-mov" id="date-debut-mov" placeholder="date de debut">
+			<input type="date" name="date-fin-mov" id="date-fin-mov" placeholder="date de fin">
+
+		</div>
+
+		<br>
+		<br>
+
+		<div id="confirm-button" name="confirm-button">
+			<input type="submit" name="confirm-button" id="confirm-button">
+		</div>
+
+	</form>
+
+</body>
+</html>
