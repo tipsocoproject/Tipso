e544c21e03713c640aa621492250d258d02df274
doooud <philippick.a@gmail.com>
2017-02-04 13:01:47 +0100

    Uniformisation du HEADER

--- Vue/mainpage.php
+++ Vue/mainpage.php
@@ -14,7 +14,8 @@
 				<meta charset="utf-8">
 				<meta name="viewport" content="width=device-width, initial-scale=1">
 				<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-				<link rel="stylesheet" href="/resources/demos/style.css">
+				<link rel="stylesheet" href="/resources/demos/style.css">    
+				<link rel="stylesheet" type="text/css" href="../Styles/header_footer_for_connected_style.css">
 				<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 				<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 				<script>
