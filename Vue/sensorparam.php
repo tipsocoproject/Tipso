da475c997a435dcbd8ae3d67d1af4a6d6e3a383b
doooud <philippick.a@gmail.com>
2017-02-04 21:54:46 +0100

    Incorporation des graphiques de température et de mouvement

--- Vue/sensorparam.php
+++ Vue/sensorparam.php
@@ -16,18 +16,98 @@ include("../Controleur/controleur_chart.php");
 	<link rel="stylesheet" type="text/css" href="../Styles/header_footer_for_connected_style.css">
 	<link rel="stylesheet" type="text/css" href="../Styles/button.css">
 	<link rel="stylesheet" type="text/css" href="../Styles/sensorparam.style.css">
+	<title>Fonctions</title>
+<?php 
+if($title == "Lumière" || $title="Mouvement")
+{
+	?>
 	<script src="../Javascript/jquery.js"></script>
 	<script src="../Javascript/Chart.js"></script>
-	<script>
-		<?php 
-		if($title=="Lumière")
-		{
-			include("../Javascript/chart-script.js");
-		}
-		?>
 
-		<title>Fonctions</title>
+	<!-- LUMIERE / MOUVEMENT -->
+	<script>
+		'use strict'
+
+		$(function () {
+
+			// Step 3. Create a data object
+			var data = {
+			    labels: <?php print json_encode($datatime);?>,
+			    datasets: [
+			        /*{
+			            label: "The First Dataset",
+			            fillColor: "rgba(153,0,76,0.2)", // magenta
+			            strokeColor: "rgba(153,0,76,1)", // magenta
+			            pointColor: "rgba(153,0,76,1)", // magenta
+			            pointStrokeColor: "#fff", // white
+			            pointHighlightFill: "#fff", // white
+			            pointHighlightStroke: "rgba(153,0,76,1)", // magenta
+			            data: [100, 34, 21, 56, 23, 90, 40]
+			        },*/
+			        {
+			            label: "The Second dataset",
+			            fillColor: "rgba(76,0,153,0.2)",
+			            strokeColor: "rgba(76,0,153,1)",
+			            pointColor: "rgba(76,0,153,1)",
+			            pointStrokeColor: "#fff", // white
+			            pointHighlightFill: "#fff", // white
+			            pointHighlightStroke: "rgba(76,0,153,1)",
+			            data: <?php print json_encode($data);?>
+			        }
+			    ]
+			};
+
+			// Step 2. Get the context of the canvas element we want to select
+			var ctx = document.getElementById("myChartlum").getContext("2d");
+			var myNewChart = new Chart(ctx).Bar(data, {
+			    ///Boolean - Whether grid lines are shown across the chart
+			    scaleShowGridLines : true,
+
+			    //String - Colour of the grid lines
+			    scaleGridLineColor : "rgba(0,0,0,.1)",
+
+			    //Number - Width of the grid lines
+			    scaleGridLineWidth : 1,
+
+			    //Boolean - Whether to show horizontal lines (except X axis)
+			    scaleShowHorizontalLines: true,
+
+			    //Boolean - Whether to show vertical lines (except Y axis)
+			    scaleShowVerticalLines: true,
+
+			    //Boolean - Whether the line is curved between points
+			    bezierCurve : true,
+
+			    //Number - Tension of the bezier curve between points
+			    bezierCurveTension : 0.4,
+
+			    //Boolean - Whether to show a dot for each point
+			    pointDot : true,
+
+			    //Number - Radius of each point dot in pixels
+			    pointDotRadius : 4,
+
+			    //Number - Pixel width of point dot stroke
+			    pointDotStrokeWidth : 1,
+
+			    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+			    pointHitDetectionRadius : 20,
+
+			    //Boolean - Whether to show a stroke for datasets
+			    datasetStroke : true,
+
+			    //Number - Pixel width of dataset stroke
+			    datasetStrokeWidth : 2,
+
+			    //Boolean - Whether to fill the dataset with a colour
+			    datasetFill : true,
+			});
+
+		});
 	</script>
+<?php
+}
+?>
 </head>
 		<?php
 
@@ -35,62 +115,61 @@ include("../Controleur/controleur_chart.php");
 		?>
 
 	<body>
-		<!--<div class="mychart">
-				<?php
-					if($title == "Lumière")
-					{
-						?>
-
-						<form method="POST" action="">
-							<canvas id="myChartlum" height="550" width="700"></canvas>
-							<select id="date" name="date">
-								<?php
-									if(isset($_POST['confirm-chart']))
-									{
-										echo '<option select="selected">'.$selectdate.'</option>';
-									}
-									else
-									{
-										echo '<option select="selected">&nbsp;</option>';
-									}
-									for ($i=0; $i<count($listedate); $i++)
-									{
-										echo "<option value=".$listedate[$i].">".$listedate[$i]."</option>";
-									}
-								?>
-							</select>
-
-							<select id="capteur" name="capteur">
-								<?php
-									if(isset($_POST['confirm-chart']))
-									{
-										echo '<option select="selected">'.$selectdate.'</option>';
-									}
-									else
-									{
-										echo '<option select="selected">&nbsp;</option>';
-									}
-									for ($i=0; $i<count($listecapteur); $i++)
-									{ 
-										echo "<option value=".$listecapteur[$i].">".$listecapteur[$i]."</option>";
-									}
-								?>
-							</select>
-							
-							<input type="submit" value="Valider" name="confirm-chart" id="confirm-button">
-						</form>
+		<div class="mychart">
+		<?php
+		if($title == "Lumière" || $title=="Mouvement")
+		{
+			?>
+			<div class="inside-chart">
+				<form method="POST" action="">
+					<canvas id="myChartlum" height="450" width="450" style="position: relative; left: 13%;"></canvas>
+					<br>
+					<br>
+					<select id="date" name="date" class="chart-input">
 						<?php
-					}
+							if(isset($_POST['confirm-chart']))
+							{
+								echo '<option select="selected">'.$selectdate.'</option>';
+							}
+							else
+							{
+								echo '<option select="selected">&nbsp;</option>';
+							}
+							for ($i=0; $i<count($listedate); $i++)
+							{
+								echo "<option value=".$listedate[$i].">".$listedate[$i]."</option>";
+							}
+						?>
+					</select>
 
-
-
-				?>
-
-
-
+					<select id="capteur" name="capteur" class="chart-input">
+						<?php
+							if(isset($_POST['confirm-chart']))
+							{
+								echo '<option select="selected">'.$selectcapteur.'</option>';
+							}
+							else
+							{
+								echo '<option select="selected">&nbsp;</option>';
+							}
+							for ($i=0; $i<count($listecapteur); $i++)
+							{ 
+								echo "<option value=".$listecapteur[$i].">".$listecapteur[$i]."</option>";
+							}
+						?>
+					</select>
+					<input type="submit" value="Valider" name="confirm-chart" id="confirm-button" class="chart-input">
+			
+				</form>
 			</div>
-		-->
-	    <div class="form" align="center">
+		<?php
+		}
+		?>
+			</div>
+		
+	    <div class="form" align="center" <?php if($title=="Lumière" || $title=="Mouvement"){
+	    	echo 'style="position: relative; bottom: 45%; right: 18%"';
+	    }?>>
 	    	<br>
 	    	<br>
 			<br>
@@ -241,6 +320,7 @@ include("../Controleur/controleur_chart.php");
 				</span>	
 			</form>
 			<br>
+		</div>
 	</body>
 
 	<?php 
